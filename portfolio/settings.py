6c7244882ec1e8f814f0874f2f743f89adbbbb62
Talha Ahmed <aditalha@gmail.com>
2021-05-12 22:44:58 +0900

    Static files fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,13 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#staticfiles dirs 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# media url for rendering images
-MEDIA_URL = '/images/'
-
-# directory for storing files by the users
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
