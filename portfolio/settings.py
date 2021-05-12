7ca1c32fb349778be5fcb50a3a12d154a14629a2
Talha Ahmed <aditalha@gmail.com>
2021-05-12 22:22:29 +0900

    Cleaning the domain name

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://talhaahmed.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'talhaahmed.herokuapp.com']
 
 
 # Application definition
